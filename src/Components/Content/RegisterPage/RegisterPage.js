e47856c8ce1aea1b3add8ba6ce05c4eae79992e6
Humza-A <humzaaamir61@gmail.com>
2023-01-15 21:17:33 -0500

    Updated: Frontend Verification Added: FontAwesome Icons

--- src/Components/Content/RegisterPage/RegisterPage.js
+++ src/Components/Content/RegisterPage/RegisterPage.js
@@ -1,11 +1,17 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCheckCircle,
+  faTimesCircle,
+  faExclamationCircle,
+} from "@fortawesome/free-solid-svg-icons";
 import { useRef, useState, useEffect } from "react";
 import { Container, Form, Button } from "react-bootstrap";
 import SimpleHeader from "../../Headers/HeaderSimple/HeaderSimple";
 import "./RegisterPage.css";
 
 export default function RegisterPage() {
-  const EMAIL_REGEX =
-    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  const EMAIL_REGEX = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  // /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
   const PWD_REGEX = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{7,15}$/;
 
   const emailRef = useRef();
@@ -18,6 +24,9 @@ export default function RegisterPage() {
   const [pwd, setPwd] = useState("");
   const [validPwd, setValidPwd] = useState(false);
   const [pwdFocus, setPwdFocus] = useState(false);
+
+  const [errMsg, setErrMsg] = useState("");
+  const [success, setSuccess] = useState(false);
 
   useEffect(() => {
     emailRef.current.focus();
@@ -37,14 +46,35 @@ export default function RegisterPage() {
     setValidPwd(validPwd);
   }, [pwd]);
 
+  useEffect(() => {
+    setErrMsg("");
+  }, [email, pwd]);
+
+  // const handleSubmit = async (e) => {
+  //   e.preventDefault();
+  //   const emailCheck = EMAIL_REGEX.test(email);
+  //   const pwdCheck = EMAIL_REGEX.test(pwd);
+  //   if (!emailCheck || !pwdCheck) {
+  //     setErrMsg("Invalid Entry");
+  //     return;
+  //   }
+
+  // };
+
   return (
     <>
       <Container fluid className="RegPageCont">
         <SimpleHeader />
         <div className="RegFormDiv">
-          <Form className="RegForm">
+          <Form action="http://localhost:3000/" className="RegForm">
             <Form.Group className="mb-3" controlId="formBasicEmail">
               <Form.Label>Email address</Form.Label>
+              <span className={validEmail ? "show checkGreen" : "hide"}>
+                <FontAwesomeIcon icon={faCheckCircle} />
+              </span>
+              <span className={validEmail || !email ? "hide" : "show crossRed"}>
+                <FontAwesomeIcon icon={faTimesCircle} />{" "}
+              </span>
               <Form.Control
                 ref={emailRef}
                 onFocus={() => setEmailFocus(true)}
@@ -56,14 +86,37 @@ export default function RegisterPage() {
               />
             </Form.Group>
 
-            <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Group
+              className={validPwd || !pwd ? "mb-3 pwdDiv" : "pwdDiv"}
+              controlId="formBasicPassword"
+            >
               <Form.Label>Password</Form.Label>
+              <span className={validPwd ? "show checkGreen" : "hide"}>
+                <FontAwesomeIcon icon={faCheckCircle} />
+              </span>
+              <span className={validPwd || !pwd ? "hide" : "show crossRed"}>
+                <FontAwesomeIcon icon={faTimesCircle} />
+              </span>
               <Form.Control
                 required
                 onChange={(e) => setPwd(e.target.value)}
                 type="password"
                 placeholder="Password"
               />
+              <span className={validPwd || !pwd ? "hide" : "show pwdInfoSpan"}>
+                <div className="pwdInfoDiv">
+                  <div className="pwdInfoIconDiv">
+                    <FontAwesomeIcon
+                      className="pwdInfoIcon"
+                      icon={faExclamationCircle}
+                    />
+                  </div>
+                  <div>
+                    - 7 to 15 characters. <br />- Must include uppercase and
+                    lowercase letters, a number and a special character.
+                  </div>
+                </div>
+              </span>
             </Form.Group>
             <div className="btns">
               <Button type="submit">Sign Up</Button>
